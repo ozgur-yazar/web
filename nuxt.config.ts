173a6f62c9b3b198e247068976317e0364c66802
M.Salih Gumus <gumusmuhammedsalih@gmail.com>
2023-05-16 21:23:34 +0300

    [Salih] add title and lang in nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-
-})
+	app: {
+		head: {
+			title: 'Özgür Yazar',
+			htmlAttrs: {
+				lang: 'tr',
+			},
+		},
+	},
+});
