6f5f8796a79bc4ec04970d486362e62d409337af
M.Salih Gumus <gumusmuhammedsalih@gmail.com>
2023-05-20 18:53:17 +0300

    [Salih] fix firabase env

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -19,63 +19,14 @@ export default defineNuxtConfig({
 	},
 
 	runtimeConfig: {
-		FB_API_KEY:
-			nodeEnv === 'PROD'
-				? process.env.NUXT_FB_API_KEY
-				: process.env.NUXT_FB_API_KEY_DEV,
-		FB_AUTH_DOMAIN:
-			nodeEnv === 'PROD'
-				? process.env.NUXT_FB_AUTH_DOMAIN
-				: process.env.NUXT_FB_AUTH_DOMAIN_DEV,
-		FB_PROJECT_ID:
-			nodeEnv === 'PROD'
-				? process.env.NUXT_FB_PROJECT_ID
-				: process.env.NUXT_FB_PROJECT_ID_DEV,
-		FB_STORAGE_BUCKET:
-			nodeEnv === 'PROD'
-				? process.env.NUXT_FB_STORAGE_BUCKET
-				: process.env.NUXT_FB_STORAGE_BUCKET_DEV,
-		FB_MESSAGING_SENDER_ID:
-			nodeEnv === 'PROD'
-				? process.env.NUXT_FB_MESSAGING_SENDER_ID
-				: process.env.NUXT_FB_MESSAGING_SENDER_ID_DEV,
-		FB_APP_ID:
-			nodeEnv === 'PROD'
-				? process.env.NUXT_FB_APP_ID
-				: process.env.NUXT_FB_APP_ID_DEV,
-		FB_MEASUREMENT_ID:
-			nodeEnv === 'PROD'
-				? process.env.NUXT_FB_MEASUREMENT_ID
-				: process.env.NUXT_FB_MEASUREMENT_ID_DEV,
 		public: {
-			FB_API_KEY:
-				nodeEnv === 'PROD'
-					? process.env.NUXT_FB_API_KEY
-					: process.env.NUXT_FB_API_KEY_DEV,
-			FB_AUTH_DOMAIN:
-				nodeEnv === 'PROD'
-					? process.env.NUXT_FB_AUTH_DOMAIN
-					: process.env.NUXT_FB_AUTH_DOMAIN_DEV,
-			FB_PROJECT_ID:
-				nodeEnv === 'PROD'
-					? process.env.NUXT_FB_PROJECT_ID
-					: process.env.NUXT_FB_PROJECT_ID_DEV,
-			FB_STORAGE_BUCKET:
-				nodeEnv === 'PROD'
-					? process.env.NUXT_FB_STORAGE_BUCKET
-					: process.env.NUXT_FB_STORAGE_BUCKET_DEV,
-			FB_MESSAGING_SENDER_ID:
-				nodeEnv === 'PROD'
-					? process.env.NUXT_FB_MESSAGING_SENDER_ID
-					: process.env.NUXT_FB_MESSAGING_SENDER_ID_DEV,
-			FB_APP_ID:
-				nodeEnv === 'PROD'
-					? process.env.NUXT_FB_APP_ID
-					: process.env.NUXT_FB_APP_ID_DEV,
-			FB_MEASUREMENT_ID:
-				nodeEnv === 'PROD'
-					? process.env.NUXT_FB_MEASUREMENT_ID
-					: process.env.NUXT_FB_MEASUREMENT_ID_DEV,
+			FB_API_KEY: process.env.NUXT_FB_API_KEY,
+			FB_AUTH_DOMAIN: process.env.NUXT_FB_AUTH_DOMAIN,
+			FB_PROJECT_ID: process.env.NUXT_FB_PROJECT_ID,
+			FB_STORAGE_BUCKET: process.env.NUXT_FB_STORAGE_BUCKET,
+			FB_MESSAGING_SENDER_ID: process.env.NUXT_FB_MESSAGING_SENDER_ID,
+			FB_APP_ID: process.env.NUXT_FB_APP_ID,
+			FB_MEASUREMENT_ID: process.env.NUXT_FB_MEASUREMENT_ID,
 		},
 	},
 });
--- plugins/firebase.client.ts
+++ plugins/firebase.client.ts
@@ -7,13 +7,13 @@ export default defineNuxtPlugin((nuxtApp) => {
 	const config = useRuntimeConfig();
 
 	const firebaseConfig = {
-		apiKey: config.FB_API_KEY,
-		authDomain: config.FB_AUTH_DOMAIN,
-		projectId: config.FB_PROJECT_ID,
-		storageBucket: config.FB_STORAGE_BUCKET,
-		messagingSenderId: config.FB_MESSAGING_SENDER_ID,
-		appId: config.FB_APP_ID,
-		measurementId: config.FB_MEASUREMENT_ID,
+		apiKey: config.public.FB_API_KEY,
+		authDomain: config.public.FB_AUTH_DOMAIN,
+		projectId: config.public.FB_PROJECT_ID,
+		storageBucket: config.public.FB_STORAGE_BUCKET,
+		messagingSenderId: config.public.FB_MESSAGING_SENDER_ID,
+		appId: config.public.FB_APP_ID,
+		measurementId: config.public.FB_MEASUREMENT_ID,
 	};
 
 	const app = initializeApp(firebaseConfig);
